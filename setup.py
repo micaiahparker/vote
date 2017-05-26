d595673467004ea1ea8645dbbfa3f2e5483c2686
Micaiah Parker <me@micaiahparker.com>
2017-05-26 02:08:43 -0400

    renamed and added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='voters',
+    version='0.1',
+    author='Micaiah Parker',
+    author_email='me@micaiahparker.com',
+    license='MIT',
+    packages=['voters'],
+    install_requires=['names']
+)
